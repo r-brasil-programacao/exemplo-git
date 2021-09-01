7dc202ca76c7f445c11bec9221a46ef93e68a605
Bruno Castro <mcastrobruno@gmail.com>
2021-09-01 22:39:47 +0100

    Inserido leitura de inputs para a funcao soma

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -7,11 +7,24 @@ namespace calculadora
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Soma();
         }
 
-        static int Soma(int a, int b)
+        static void Soma()
         {
-            return a + b;
+            System.Console.WriteLine("******* Soma ****** ");
+            System.Console.WriteLine("Digite o primeiro input: ");
+            var n1Input = Console.ReadLine();
+            System.Console.WriteLine("Digite o segunda input: ");
+            var n2Input = Console.ReadLine();
+
+
+            int n1 = int.Parse(n1Input);
+            int n2 = int.Parse(n2Input);
+
+            int resultado = n1 + n2;
+            System.Console.WriteLine("Resultado da Soma é: " + resultado);
         }
     }
 }
