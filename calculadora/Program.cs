ca251528e527be67d144878736277644b6934082
Bruno Castro <mcastrobruno@gmail.com>
2021-09-01 22:46:05 +0100

    Funcao soma (#1)
    * Criada funcao de soma
    
    * Inserido leitura de inputs para a funcao soma

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -7,6 +7,24 @@ namespace calculadora
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Soma();
+        }
+
+        static void Soma()
+        {
+            System.Console.WriteLine("******* Soma ****** ");
+            System.Console.WriteLine("Digite o primeiro input: ");
+            var n1Input = Console.ReadLine();
+            System.Console.WriteLine("Digite o segunda input: ");
+            var n2Input = Console.ReadLine();
+
+
+            int n1 = int.Parse(n1Input);
+            int n2 = int.Parse(n2Input);
+
+            int resultado = n1 + n2;
+            System.Console.WriteLine("Resultado da Soma é: " + resultado);
         }
     }
 }
